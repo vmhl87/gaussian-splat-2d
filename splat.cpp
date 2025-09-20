49185b23a7851c3de81db011efdad8b90e1d9077
Vincent Loh <vincentmloh87@gmail.com>
2025-09-20 10:17:37 -0400

    condense painting logic into one file

--- splat.cpp
+++ splat.cpp
@@ -2,6 +2,7 @@ const int VERSION = 1;
 
 #include <sstream>
 #include <fstream>
+#include <thread>
 
 struct _splat{
 	double x, y, r[4], c[4];
@@ -14,6 +15,57 @@ int iter = 0;
 unsigned char *data2;
 int width, height;
 double *canvas;
+
+void _paint(int xmin, int ymin, int xmax, int ymax){
+	for(int i=0; i<n; ++i){
+
+		double R = std::max(splat[i].r[0], splat[i].r[1]);
+
+		int Xmin = std::max(xmin,
+				(int) std::floor(splat[i].x-R*3.0)),
+			Xmax = std::min(xmax,
+				(int) std::ceil(splat[i].x+R*3.0)),
+			Ymin = std::max(ymin,
+				(int) std::floor(splat[i].y-R*3.0)),
+			Ymax = std::min(ymax,
+				(int) std::ceil(splat[i].y+R*3.0));
+
+		for(int y=Ymin; y<Ymax; ++y){
+			for(int x=Xmin; x<Xmax; ++x){
+				int j = 3*(x+y*width);
+
+				double rx = splat[i].x-x, ry = splat[i].y-y;
+				double vx = (rx*splat[i].r[2]+ry*splat[i].r[3])/splat[i].r[0],
+					   vy = (ry*splat[i].r[2]-rx*splat[i].r[3])/splat[i].r[1];
+				double alpha = exp(-vx*vx-vy*vy) * splat[i].c[3];
+
+				for(int z=0; z<3; ++z)
+					canvas[j+z] = canvas[j+z]*(1.0-alpha) + splat[i].c[z]*alpha;
+			}
+		}
+	}
+}
+
+void paint(){
+	for(int i=0; i<width*height*3; ++i) canvas[i] = 1.0;
+	
+	int nthreads = 10;
+	std::thread threads[nthreads-1];
+
+	for(int i=1; i<nthreads; ++i)
+		threads[i-1] = std::thread(_paint, 0, height*(i-1)/nthreads, width, height*i/nthreads);
+
+	_paint(0, height*(nthreads-1)/nthreads, width, height);
+	
+	for(int i=1; i<nthreads; ++i) threads[i-1].join();
+}
+
+void write(const char *filename){
+	for(int i=0; i<width*height*3; ++i)
+		data2[i] = (unsigned char) std::max(0, std::min(255, (int) std::floor(255.0 * canvas[i])));
+
+	write_bmp(filename, data2, width, height);
+}
 
 int read_splats(const char *filename){
     std::ifstream F(filename);
