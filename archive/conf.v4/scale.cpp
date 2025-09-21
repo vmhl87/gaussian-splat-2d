f129bd4743a36f9f567004707524c355a4988549
Vincent Loh <vincentmloh87@gmail.com>
2025-09-21 17:53:05 -0400

    on second thought PSO is probably not effective - stashing code just in case

---
+++ archive/conf.v4/scale.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cmath>
+
+#include "src/write_bmp.cpp"
+#include "src/read_bmp.cpp"
+
+#include "splat.cpp"
+
+int main(int argc, const char **argv){
+	if(argc < 4){
+		std::cerr << "Incorrect usage:\n";
+		std::cerr << argv[0] << " <config.loc> <output.loc> <scale>\n";
+		return 0;
+	}
+
+	int scale = std::stoi(argv[3]);
+
+	read_splats(argv[1]);
+
+	if(argc >= 5){
+		float S = std::stoi(argv[4])/1e2;
+		for(int i=0; i<n; ++i){
+			splat[i].r[0] *= S;
+			splat[i].r[1] *= S;
+		}
+	}
+
+	width *= scale, height *= scale;
+	data2 = new unsigned char[width*height*3];
+	canvas = new float[width*height*3];
+
+	for(int i=0; i<n; ++i){
+		splat[i].x *= scale, splat[i].y *= scale;
+		splat[i].r[0] *= scale, splat[i].r[1] *= scale;
+	}
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	paint();
+
+	auto now = std::chrono::high_resolution_clock::now();
+	float duration = ((std::chrono::duration<float>) (now-start)).count();
+	std::cout << "upscaled in " << (int) std::floor(duration*1000) << "ms\n";
+
+	write(argv[2]);
+}
---
+++ render.cpp
@@ -0,0 +1,55 @@
+#include <sstream>
+#include <fstream>
+#include <thread>
+
+void _paint(_splat *splat, int xmin, int ymin, int xmax, int ymax){
+	for(int i=0; i<n; ++i){
+
+		auto SP = [] (float a, float b) { return std::sqrt(a*a+b*b); };
+		float xr = SP(splat[i].r[0]*splat[i].r[2], splat[i].r[1]*splat[i].r[3]),
+			  yr = SP(splat[i].r[0]*splat[i].r[3], splat[i].r[1]*splat[i].r[2]);
+
+		xr *= 2.2;
+		yr *= 2.2;
+
+		int Xmin = std::max(xmin, (int) std::floor(splat[i].x-xr)),
+			Xmax = std::min(xmax, (int) std::ceil(splat[i].x+xr)),
+			Ymin = std::max(ymin, (int) std::floor(splat[i].y-yr)),
+			Ymax = std::min(ymax, (int) std::ceil(splat[i].y+yr));
+
+		for(int y=Ymin; y<Ymax; ++y){
+			for(int x=Xmin; x<Xmax; ++x){
+				int j = 3*(x+y*width);
+
+				float rx = splat[i].x-x, ry = splat[i].y-y;
+				float vx = (rx*splat[i].r[2]+ry*splat[i].r[3])/splat[i].r[0],
+					  vy = (ry*splat[i].r[2]-rx*splat[i].r[3])/splat[i].r[1];
+				float alpha = exp(-vx*vx-vy*vy) * splat[i].c[3];
+
+				for(int z=0; z<3; ++z)
+					canvas[j+z] = canvas[j+z]*(1.0-alpha) + splat[i].c[z]*alpha;
+			}
+		}
+	}
+}
+
+void paint(_state &s){
+	for(int i=0; i<width*height*3; ++i) canvas[i] = 1.0;
+	
+	int nthreads = 10;
+	std::thread threads[nthreads-1];
+
+	for(int i=1; i<nthreads; ++i)
+		threads[i-1] = std::thread(_paint, s.data, 0, height*(i-1)/nthreads, width, height*i/nthreads);
+
+	_paint(s.data, 0, height*(nthreads-1)/nthreads, width, height);
+	
+	for(int i=1; i<nthreads; ++i) threads[i-1].join();
+}
+
+void write(const char *filename){
+	for(int i=0; i<width*height*3; ++i)
+		data2[i] = (unsigned char) std::max(0, std::min(255, (int) std::floor(255.0 * canvas[i])));
+
+	write_bmp(filename, data2, width, height);
+}
